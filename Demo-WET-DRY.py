c82ce49fd511196b98defb7bdef76c38ad42441a
tin-mzl <muzoladestine3@gmail.com>
2026-02-01 23:54:07 +0100

    Update Demo-WET-DRY.py

--- Demo-WET-DRY.py
+++ Demo-WET-DRY.py
@@ -1,22 +1,24 @@
 #la table de multiplication par 5
-print (5*0)
-print (5*1)
-print(5*2)
-print (5*3)
-print(5*4)
-print (5*5)
-print(5*6)
-print (5*7)
-print(5*8)
-print (5*9)
-print(5*10)
+print("Table de multiplication par 5")
+print (f"5x0 = {5*0}")
+print (f"5x1 = {5*1}")
+print(f"5x2 = {5*2}")
+print (f"5x3 = {5*3}")
+print(f"5x4 = {5*4}")
+print (f"5x5 = {5*5}")
+print(f"5x6 = {5*6}")
+print (f"5x7 = {5*7}")
+print(f"5x8 = {5*8}")
+print (f"5x9 = {5*9}")
+print(f"5x10 = {5*10}")
 #si on veut par exemple changer de nombre on sera obliger de le faire manuellement
 #donc il faut automatiser tout ça
 #l'automatisation de la tache
 def table_de_multiplication(n):
-    for i  in range(0,11):
+  print(f"la table de multiplication {n}")
+  for i  in range(0,11):
         print(f"{n} x {i} = {i*n}")
-    return i * n
+  return i * n
 #On cree une variable number pour demander a l'utilisateur quel table de multiplication il veut
 num = int(input("veillez saisir un nombre: "))
 
